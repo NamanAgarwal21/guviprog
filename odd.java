c01f74e68684873a05401818ede7001e802ddf18
Naman Agarwal <namanagarwal407@gmail.com>
2018-12-04 01:25:13 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class odd
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n<0)
+{System.out.println("invalid");
+}
+else
+{
+if(n%2==0){
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}}}
+
